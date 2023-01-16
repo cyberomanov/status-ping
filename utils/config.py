9c90590d5b17be731dd58d533cccab6988c81956
cyberomanov <romanoff_1@bk.ru>
2023-01-17 02:37:37 +0400

    v2.8

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,10 @@
+import os
+
 from yaml import safe_load
 
 from datatypes.config import Config
 
 
-def get_config(config_path: str = './config.yaml') -> Config:
+def get_config(config_path: str = r"%s/config.yaml" % os.getcwd()) -> Config:
     with open(config_path, encoding='UTF-8') as data:
         return Config.parse_obj(safe_load(data))
